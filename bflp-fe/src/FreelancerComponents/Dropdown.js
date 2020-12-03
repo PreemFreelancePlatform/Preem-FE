77f5d7b0ebf706917d324f0c00dfed5e6b2d913c
unknown <william.z.wilson95@gmail.com>
2020-12-02 19:09:04 -0800

    lots

--- bflp-fe/src/FreelancerComponents/Dropdown.js
+++ bflp-fe/src/FreelancerComponents/Dropdown.js
@@ -1,31 +1,29 @@
 import React, { useState } from 'react';
-import showless from '../assets/expand_less-24px.svg';
-import showmore from '../assets/expand_more-24px.svg';
 
 export const Dropdown = ({ selection, setSelection }) => {
-	const [open, setOpen] = useState(false);
-	const arr = ['Web', 'Mobile', 'Dev-ops', 'Systems'];
+	// const [open, setOpen] = useState(false);
 
-	const handleClick = (item) => {
-		setSelection(item);
-		setOpen(false);
-	};
+	// const handleClick = (item) => {
+	// 	setSelection(item);
+	// 	setOpen(false);
+	// };
 
 	return (
 		<div className="dropdown">
 			<p>{`(${selection})`} </p>
-			<ul className={open ? 'show-dropdown' : 'hide-dropdown'}>
-				{arr.map((item, index) => (
-					<li
-						onClick={() => {
-							handleClick(item);
-						}}
-						key={index}
-					>
-						{selection === item ? '' : item}
-					</li>
-				))}
-			</ul>
 		</div>
 	);
 };
+
+// <ul className={open ? 'show-dropdown' : 'hide-dropdown'}>
+// 	{arr.map((item, index) => (
+// 		<li
+// 			onClick={() => {
+// 				handleClick(item);
+// 			}}
+// 			key={index}
+// 		>
+// 			{selection === item ? '' : item}
+// 		</li>
+// 	))}
+// </ul>
